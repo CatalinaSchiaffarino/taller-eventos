5ad117c1132a28202056fb199662334b6a787bee
catalinaschiaffarino <catalinaschiaffarinomartinez@gmail.com>
2024-08-13 12:02:01 -0300

    Update eventos.js

--- eventos.js
+++ eventos.js
@@ -1,3 +1,7 @@
 document.getElementById("miDiv").addEventListener("click", function() {
     alert("Hola! Soy el div");
 });
+document.getElementById("miBoton").addEventListener("click", function(event) {
+    alert('Hola!');
+    event.stopPropagation(); 
+});
